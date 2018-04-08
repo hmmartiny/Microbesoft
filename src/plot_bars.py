8c6b696645c2a7ef65106fbd18ea047cb5d2370f
Christian <s134801@student.dtu.dk>
2018-04-08 10:50:46 +0200

    added args.same_length

--- src/plot_bars.py
+++ src/plot_bars.py
@@ -25,7 +25,8 @@ def plot_bars(image, values, color, x, y, width, height):
 
     im = Image.open('consensus.png')
     # remove border from invisible axis
-    im = im.crop((4, 4, im.width - 4, im.height - 4))
+    crop = 2
+    im = im.crop((crop, crop, im.width - crop, im.height - crop))
     # resize to the wanted size
     im = im.resize((width, height), Image.ANTIALIAS)
     image.paste(im, (x, y))
